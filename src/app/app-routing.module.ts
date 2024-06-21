f45fb18bad617723efde293078dfeb039c7ce2a2
lindasDroppa <139218024+lindasDroppa@users.noreply.github.com>
2024-06-21 23:26:48 +0200

    UI changed to white

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import {RouterModule, Routes} from "@angular/router";
-import {NgModule} from "@angular/core";
-import {NotfoundComponent} from "./notfound/notfound.component";
-
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.component').then((m) => m.HomeComponent) },
-  { path:'',loadChildren:() => import('./home/home.component').then((m) => m.HomeComponent)},
-  { path: '**', component: NotfoundComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to 'home' when path is empty
+  { path: '**', component: NotfoundComponent } // Wildcard route for 404 Not Found
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-    }),
+      scrollPositionRestoration: 'enabled', // Enable scroll restoration
+    })
   ],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
